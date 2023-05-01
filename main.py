8f2d9a4210b33d8cff4405327a6405bfae75dfe2
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-01 14:26:24 +0300

    thrfgb

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import time
-
-def delay_decorator(func):
-    def wrapper(*args, **kwargs):
-        time.sleep(3)
-        return func(*args, **kwargs)
-    return wrapper
-
-@delay_decorator
-def say_hello():
-    print("Hello")
-
-say_hello()
+yhtg
